98ce618e353f5d3e8c1f4d0ea7a1bfabeb59c858
Santiago del Castillo <sjdelcastilloc@gmail.com>
2024-02-13 14:44:09 -0400

    worked on Node and Graph

--- src/antcolonyoptimization/Node.java
+++ src/antcolonyoptimization/Node.java
@@ -8,6 +8,17 @@ package antcolonyoptimization;
  *
  * @author Santiago
  */
-public class Node {
-    
+public class Node<T> {
+    T data;
+    Node<T>[] adjNodes;
+    int index = 0;
+
+    public Node(T data, int size) {
+        this.data = data;
+        this.adjNodes = new Node[size];
+    }
+
+    public void addAdjNode(Node<T> node) {
+        adjNodes[index++] = node;
+    }
 }
